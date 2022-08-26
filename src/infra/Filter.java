9ad314be05072e9b44990e752256ab359e156439
gimyongjoo <dydwn2432@naver.com>
2022-08-26 16:15:43 +0900

    Filter기능 구현

--- src/infra/Filter.java
+++ src/infra/Filter.java
@@ -33,4 +33,39 @@ public class Filter {
         return false;
     }
 
+    private String sorting(String url){
+
+        if(isAnonymous(url)){
+            return "ANONYMOUS";
+        }
+
+        if(isNeedAuth(url)){
+            return "HASAUTH";
+        }
+
+        return "PERMITALL";
+
+    }
+
+    public boolean isValidRequest(){
+
+        String originUrl = request.getOriginUri();
+
+        String sortCode = sorting(originUrl);
+        boolean isLogon = request.isLogon();
+
+        if(sortCode.equals("ANONYMOUS")){
+            if(isLogon){
+                return false;
+            }
+        }else if(sortCode.equals("HASAUTH")){
+            if(!isLogon){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
 }
